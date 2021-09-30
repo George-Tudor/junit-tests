5859c0c2347310606e14c039c61f7436cb8380c3
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-30 14:27:22 -0500

    add partial completion of exercise 1

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -9,39 +9,36 @@ public class Student {
     public Student(String name, long id) {
         this.name = name;
         this.id = id;
+        this.grades = new ArrayList<>();
     }
 
     // returns the student's id
     public long getId(){
-
+        return this.id;
     }
 
     // returns the student's name
     public String getName(){
-
+        return this.name;
     }
 
     // adds the given grade to the grades list
     public void addGrade(int grade){
-
+        this.grades.add(grade);
     }
 
     // returns the list of grades
     public ArrayList<Integer> getGrades(){
-
+        return this.grades;
     }
 
     // returns the average of the students grades
     public double getGradeAverage(){
-
-    }
-
-    public void updateGrade(){
-
-    }
-
-    public void deleteGrade(){
-
+        double sum = 0;
+        for (int grade: this.grades) {
+            sum += grade;
+        }
+        return sum / this.grades.size();
     }
 
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+    @Test
+    public void testCreateStudent() {
+        Student george = new Student("George", 1L);
+        Student bill = null;
+        assertNull(bill);
+        assertNotNull(george);
+    }
+
+    @Test
+    public void testStudentFields() {
+        Student george = new Student("George", 1L);
+
+    }
 }
