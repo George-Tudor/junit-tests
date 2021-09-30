51b871b10d9849e9a37216ea4d81f61351fb80b3
George Tudor <Alexander.George.Tudor@gmail.com>
2021-09-30 11:44:56 -0500

    add part 1 of exercise

---
+++ src/main/java/Student.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // returns the student's id
+    public long getId(){
+
+    }
+
+    // returns the student's name
+    public String getName(){
+
+    }
+
+    // adds the given grade to the grades list
+    public void addGrade(int grade){
+
+    }
+
+    // returns the list of grades
+    public ArrayList<Integer> getGrades(){
+
+    }
+
+    // returns the average of the students grades
+    public double getGradeAverage(){
+
+    }
+
+}
